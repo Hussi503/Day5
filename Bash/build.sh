688b80d9fac49861f4ea6db9a5feeecced4a9700
Hussi503 <86091094+Hussi503@users.noreply.github.com>
2025-09-25 16:52:33 +0530

    Update build.sh

--- Bash/build.sh
+++ Bash/build.sh
@@ -5,6 +5,7 @@ sudo mkdir gold
 cd gold/
 sudo git clone https://github.com/Hussi503/Day5.git
 cd Day5/Code
-sudo docker build -t react-microk8s -f golddockerfile .
+sudo docker build -t react-microk8s -f golddockerfile .sudo docker login -u hussi503 -p Hussi@503
+sudo docker login -u hussi503 -p Hussi@503
 sudo docker tag react-microk8s:latest hussi503/sagar-k-practice:latest ##make sure you did docker login
 sudo docker push hussi503/sagar-k-practice:latest
